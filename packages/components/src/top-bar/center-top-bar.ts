d839b8487ae815e8bf46bff3e93bfa7f4b4d71c8
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-03-06 10:12:31 +0330

    fix(components): eslint issues

--- packages/components/src/top-bar/center-top-bar.ts
+++ packages/components/src/top-bar/center-top-bar.ts
@@ -1,16 +1,17 @@
 import { html, nothing } from 'lit/html.js';
 
+import { endIconListTemplate } from './_end-icon-list.js';
 import {
   iconButton
 } from '../icon-button/icon-button.js';
 
-import { endIconListTemplate } from './_end-icon-list.js';
 
 import type { TopBarContent } from './_type.js';
 
 export const gecutCenterTopBar = (content: TopBarContent) =>
   html`<header
-    class="scroll z-sticky flex h-16 shrink-0 grow-0 select-none items-center bg-surface px-4 [&.scroll]:bg-surfaceContainer"
+    class="scroll z-sticky flex h-16 shrink-0 grow-0 select-none items-center
+           bg-surface px-4 [&.scroll]:bg-surfaceContainer"
   >
     ${content.startIcon ? iconButton(content.startIcon) : nothing}
 
