e470e6af50ae89f4d72be5e0319dc8e0a5cbdce5
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-03-06 09:39:59 +0330

    feat(styles): new package

---
+++ packages/styles/src/index.ts
@@ -0,0 +1,29 @@
+import { animationTheme } from './configs/animation.js';
+import { screenTheme } from './configs/screen.js';
+import { typographyTheme } from './configs/typography.js';
+import { zIndexTheme } from './configs/z-index.js';
+import { colorTheme, colorPlugin } from './plugins/color-scheme.js';
+import { elevationPlugin } from './plugins/elevation.js';
+import { stateLayerPlugin } from './plugins/state-layer.js';
+import { translucentPlugin } from './plugins/translucent.js';
+
+import type { Config } from 'tailwindcss';
+
+export const tailwindConfig: Omit<Config, 'content'> = {
+  darkMode: 'media',
+  theme: {
+    extend: {
+      ...colorTheme,
+      ...typographyTheme,
+      ...zIndexTheme,
+      ...screenTheme,
+      ...animationTheme
+    }
+  },
+  plugins: [
+    colorPlugin,
+    elevationPlugin,
+    stateLayerPlugin,
+    translucentPlugin
+  ]
+};
